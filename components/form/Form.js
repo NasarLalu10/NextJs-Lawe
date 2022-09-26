76b87e0dad783988ef5cbdd069eea88d6b0cb0e6
NasarLalu <nasar@benfy.co>
2022-09-26 10:55:12 +0530

    form-created

--- components/form/Form.js
+++ components/form/Form.js
@@ -1,15 +1,24 @@
+import { useState } from "react";
 const Form = () => {
+    const [count, setCount] = useState(0);
 
+    let add=()=>{
+        setCount(count + 1);
+    }
+    let sub=()=>{
+        setCount(count - 1);
+    }
     return (
+
         <>
-            <form>
+            {/* <>
                 <div className="container">
                     <div className="row align-items-center my-5">
                         <div className="col-md-6">
 
                             <div className="mb-3">
                                 <label for="exampleInputEmail1" className="form-label">Email address</label>
-                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                <input type="email" onChange={(event) => setState({email: event.target.value})} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                             </div>
 
                             <div className="mb-3">
@@ -28,14 +37,26 @@ const Form = () => {
                             </div>
 
 
-                            <button type="submit" className="btn btn-primary ">Submit</button>
+                            <button type="submit" onClick={PrintData} className="btn btn-primary ">Submit</button>
                             <button type="submit" className="btn btn-primary mx-3">Update</button>
                             <button type="submit" className="btn btn-primary mx-3">Delete</button>
                             <button type="submit" className="btn btn-primary mx-3">View</button>
                         </div>
+                        <div className="col-md-6">
+                            <h1>{displayData }</h1>
+                        </div>
                     </div>
                 </div>
-            </form>
+            </> */}
+
+            <div >
+
+                <button type="submit" className="btn btn-primary m-3" onClick={ add }>ADD</button>
+                <button type="submit" className="btn btn-primary m-3" onClick={ sub }>SUB</button>
+                <h1>{count}</h1>
+
+            </div>
+
         </>
     )
 }
